7dfd1cbfb5d165ff9c8232ab7b584ce6d24cd6a4
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-03-08 00:03:03 +0500

    added cors

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const app = express();
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 //config
 const port = 4000 || process.env.PORT;
 const uri = `mongodb://${process.env.MONGO_DB_ID}:${process.env.MONGO_DB_PW}@ds155845.mlab.com:55845/gql-author-app`;
 
 mongoose.connect(uri, { useNewUrlParser: true }, (err) => (err != null ? console.log(err) : ''));
 mongoose.connection.once('open', () => console.log('Mongoose connected '));
+//cors middlewar
+app.use(cors());
 //graphql-express middleware
 app.use(
 	'/graphql',
